ffb078032f4ee19caa0af4c07d18a178eabe3583
abelmalu <abellmalu@gmail.com>
2026-01-09 14:56:27 +0300

    feat:add create post

--- internal/posts/service.go
+++ internal/posts/service.go
@@ -1,1 +1,48 @@
 package todo
+
+import (
+	"log"
+	"net/http"
+	"github.com/abelmalu/golang-posts/internal/models"
+	"github.com/abelmalu/golang-posts/pkg"
+	"github.com/gin-gonic/gin"
+)
+
+
+
+
+func CreatePost(c *gin.Context){
+
+	var post models.Post
+
+	if err:=c.ShouldBindJSON(&post); err != nil{
+
+		log.Printf("JSON binding error %v",err)
+		c.JSON(http.StatusBadRequest,gin.H{
+			"status":"error",
+			"message":"Invalid request body",
+		
+		})
+	}
+
+	query := `INSERT INTO posts (title,content,user_id) VALUES($1,$2,$3)`
+
+
+	_,err := pkg.DB.Exec(query,post.Title,post.Content,post.UserID)
+
+	if err != nil{
+		log.Printf("database exec error %v",err)
+		c.JSON(http.StatusInternalServerError,gin.H{
+			"status":"error",
+			"message":"Internal Server Error",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"status":"success",
+		"message":"Successfully created a post",
+	})
+
+
+}
