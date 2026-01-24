ad985d755485401963969f19461d562e6287b33e
abel <abellmalu@gmail.com>
2026-01-24 21:59:17 +0300

    refactor:setup routes update

--- internal/routes.go
+++ internal/routes.go
@@ -1,12 +1,13 @@
 package internal
 
 import (
-	"github.com/abelmalu/golang-posts/internal/middleware"
-	"github.com/abelmalu/golang-posts/internal/posts"
-	"github.com/abelmalu/golang-posts/internal/auth"
-	"github.com/gin-gonic/gin"
 	"time"
+
+	"github.com/abelmalu/golang-posts/internal/auth"
+	"github.com/abelmalu/golang-posts/internal/middleware"
+	post "github.com/abelmalu/golang-posts/internal/posts"
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes() *gin.Engine {
@@ -15,36 +16,41 @@ func SetupRoutes() *gin.Engine {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization","X-Client-Type"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-Client-Type"},
 		ExposeHeaders:    []string{"Authorization"}, // IMPORTANT
-		AllowCredentials: true, // REQUIRED for cookies
+		AllowCredentials: true,                      // REQUIRED for cookies
 		MaxAge:           12 * time.Hour,
 	}))
-
-
 
 	authGroup := r.Group("/")
 	{
 		authGroup.POST("/register", auth.Register)
 		authGroup.POST("/login", auth.Login)
-		authGroup.POST("/refresh",auth.RefreshHandler)
-		authGroup.POST("/logout",middleware.AuthMiddleware(),auth.Logout)
-		
+		authGroup.POST("/refresh", auth.RefreshHandler)
+		authGroup.POST("/logout", middleware.AuthMiddleware(), auth.Logout)
+
 	}
 	authenticated := r.Group("/")
-	
-	authenticated.Use(middleware.AuthMiddleware(),middleware.AuthorizeRoles("users"))
-	{
-		
-		authenticated.POST("/posts", posts.CreatePost)
-		authenticated.GET("/posts",posts.GetPosts)
 
-		postOwner := authenticated.Group("/posts/:id")
-		postOwner.Use(middleware.AuthorizePostOwner())
-		postOwner.PUT("",posts.UpdatePost)
-		postOwner.DELETE("",posts.DeletePost)
+	authenticated.Use(middleware.AuthMiddleware(), middleware.AuthorizeRoles("users"))
+	{
+
+		posts := authenticated.Group("/posts")
+		{
+			posts.POST("", post.CreatePost)
+			posts.GET("", post.GetPosts)
+
+		}
+
+		postOwner := posts.Group("/:id")
+		{
+
+			postOwner.Use(middleware.AuthorizePostOwner())
+			postOwner.PUT("", post.UpdatePost)
+			postOwner.DELETE("", post.DeletePost)
+
+		}
 	}
-		
 
 	return r
 }
