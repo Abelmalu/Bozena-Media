4a2c8770fc722776013eac1c28b1909a18869134
abelmalu <abellmalu@gmail.com>
2026-01-29 16:11:10 +0300

    feat:create post proto file

--- post/cmd/main.go
+++ post/cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"net"
+
+	"github.com/abelmalu/golang-posts/post/proto/pb"
+	"google.golang.org/grpc"
+)
+type postServer struct {
+  pb.UnimplementedPostServiceServer
+}
+
+func (s *postServer) CreatePost(ctx context.Context, req *pb.CreatePostRequest) (*pb.CreatePostResponse, error) {
+    // extract user_id from context (JWT)
+    // insert into DB
+    return &pb.CreatePostResponse{
+        Status:  "success",
+        Message: "Post created",
+    }, nil
+}
+
+func main() {
+    lis, _ := net.Listen("tcp", ":50051")
+    s := grpc.NewServer()
+    pb.RegisterPostServiceServer(s, &postServer{})
+    s.Serve(lis)
+}
