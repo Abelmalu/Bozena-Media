1c21df8c509eb89e875e0c37a4bd4bb7bcad99e8
abel <abellmalu@gmail.com>
2026-02-09 22:36:33 +0300

    feat:authentication and post refactored into micorservices

--- post/internal/models/post_model.go
+++ post/internal/models/post_model.go
@@ -9,3 +9,11 @@ type Post struct {
 	UserID  int    `json:"user_id" db:"user_id" validate:"required"`
 }
 
+//likes model
+type Like struct{
+    Id  int `json:"id" db:"id"`
+	UserID int `json:"user_id" db:"user_id" validate:"required,gt=0"`
+	PostID int `json:"post_id"  db:"post_id" validate:"required,gt=0"`
+
+	
+}
