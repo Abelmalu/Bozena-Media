56a27b50c0418f63ac90c41f9415291272d1a726
abel malu <abellmalu@gmail.com>
2026-05-16 16:26:40 +0300

    refactor pkg folder

---
+++ post/pkg/utils/context_utils.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"context"
+
+	ierrors "github.com/abelmalu/golang-posts/post/internal/errors"
+	"google.golang.org/grpc/metadata"
+)
+
+
+func GetRequestID(c context.Context) (string,error){
+	var requestID string
+
+	md, exists := metadata.FromIncomingContext(c)
+
+	if !exists {
+		return "", ierrors.ErrMetaDataNotFound
+	}
+	values := md.Get("requestID")
+	if len(values) > 0 {
+		requestID = values[0]
+	} else {
+
+		return "", ierrors.ErrRequestIDNotFound
+
+	}
+	return requestID,nil
+
+
+}
