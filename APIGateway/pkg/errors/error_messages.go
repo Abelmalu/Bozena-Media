9ee95867422ad5b6d969272ba150dead986a73f6
abel malu <abellmalu@gmail.com>
2026-05-16 22:02:20 +0300

    fix:issue

---
+++ APIGateway/pkg/errors/error_messages.go
@@ -0,0 +1,19 @@
+package ierrors
+ 
+
+type ErrorMessage string
+
+const (
+	
+MSGInvalidRequestBody ErrorMessage = "Invalid Request Body"
+MSGInternalServerError ErrorMessage = "Internal Server Error"
+MSGUnauthorizedAccess ErrorMessage = "Unauthorized"
+MSGSomethingWentWrong ErrorMessage = "Something Went Wrong"
+MSGRefreshTokenNotFound ErrorMessage = "Refresh token not found"
+MSGUsernameIsRequired ErrorMessage = "Username is Required"
+MSGPathParamError          ErrorMessage = "path parameter can not be less than zero"
+
+
+
+
+)
---
+++ APIGateway/pkg/utils/context_utils.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"errors"
+
+	ierrors "github.com/abelmalu/golang-posts/APIGateway/pkg/errors"
+	"github.com/abelmalu/golang-posts/platform"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func GetRequestID(c *gin.Context, logger *platform.Logger) (string, error) {
+
+	requestID, ok := c.Get("request_id")
+	if !ok {
+		logger.Error("couldn't get request ID from context", zap.Error(errors.New("couldn't find request ID")))
+		c.Error(ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil))
+
+		return "", ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil)
+	}
+	requestIDValue, ok := requestID.(string)
+	if !ok {
+
+		logger.Error("couldn't assert the request ID to string", zap.String("type", "something went wrong"))
+		c.Error(ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil))
+		return "", ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil)
+
+	}
+
+	return requestIDValue, nil
+
+}
+
+func GetUserID(c *gin.Context, logger *platform.Logger) (int, error) {
+
+	var userID interface{}
+	var ok bool
+
+	if userID, ok = c.Get("userID"); ok {
+
+		if userIDValue, ok := userID.(int); ok {
+
+			return userIDValue, nil
+
+		} else {
+
+			logger.Error("couldn't assert the request ID to string", zap.String("type", "something went wrong"))
+			c.Error(ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil))
+			return 0, ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil)
+
+		}
+
+	} else {
+
+		logger.Error("couldn't get request ID", zap.Error(errors.New("couldn't find request ID")))
+		c.Error(ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil))
+
+		return 0, ierrors.NewInternalError(ierrors.MSGSomethingWentWrong, nil)
+
+	}
+
+}
